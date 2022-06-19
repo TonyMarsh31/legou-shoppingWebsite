7202e4329cb5e0d075430d22f01117161002de9d
tony <tonymarsh0331@gmail.com>
2022-06-19 17:08:41 +0800

    :white_check_mark: 配置商场项目的RBAC模块:legou-security为资源服务器,并进行了测试

---
+++ legou-parent/legou-security/legou-security-service/src/main/java/com/lxs/legou/security/config/ResourceServerConfiguration.java
@@ -0,0 +1,35 @@
+package com.lxs.legou.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)//激活方法上的PreAuthorize注解
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private final TokenStore tokenStore;
+
+    public ResourceServerConfiguration(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**", "/security/user/**").permitAll()
+                .antMatchers("/book/**").hasRole("ADMIN") //用于测试
+                .antMatchers("/**").authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore);
+    }
+}
