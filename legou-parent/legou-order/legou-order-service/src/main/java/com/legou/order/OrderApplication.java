7893238b1e8522ac541fb94c8295240aa5d33e0a
tony <tonymarsh0331@gmail.com>
2022-06-28 16:21:51 +0800

    :beers: 向容器中注入IdWorker组件，用于生成订单编号

--- legou-parent/legou-order/legou-order-service/src/main/java/com/legou/order/OrderApplication.java
+++ legou-parent/legou-order/legou-order-service/src/main/java/com/legou/order/OrderApplication.java
@@ -1,10 +1,12 @@
 package com.legou.order;
 
+import com.lxs.legou.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,4 +17,10 @@ public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+
 }
