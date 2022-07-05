d67356624ff8bbf1265c7ef689716074104ff8c9
tony <tonymarsh0331@gmail.com>
2022-07-05 23:55:08 +0800

    :beers: 秒杀微服务，Service层实现初步的下单逻辑实现

--- legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/service/ISeckillOrderService.java
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/service/ISeckillOrderService.java
@@ -5,5 +5,14 @@ import com.lxs.legou.seckill.po.SeckillOrder;
 
 public interface ISeckillOrderService extends ICrudService<SeckillOrder> {
 
+    /**
+     * 添加秒杀订单
+     * @param id:商品ID
+     * @param time:商品秒杀开始时间
+     * @param username:⽤户登录名
+     * @return 订单是否添加成功
+     */
+    Boolean add(Long id, String time, String username);
+
 
 }
