5edc458145200cc84c8fbcb6c5dddb8f2639f318
tony <tonymarsh0331@gmail.com>
2022-07-05 20:11:42 +0800

    :tada: Service层中添加秒杀订单的接口和实现类，暂未实现相关方法

---
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/service/ISeckillOrderService.java
@@ -0,0 +1,9 @@
+package com.lxs.legou.seckill.service;
+
+import com.lxs.legou.core.service.ICrudService;
+import com.lxs.legou.seckill.po.SeckillOrder;
+
+public interface ISeckillOrderService extends ICrudService<SeckillOrder> {
+
+
+}
---
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/service/impl/SeckillOrderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lxs.legou.seckill.service.impl;
+
+import com.lxs.legou.core.service.impl.CrudServiceImpl;
+import com.lxs.legou.seckill.po.SeckillOrder;
+import com.lxs.legou.seckill.service.ISeckillOrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeckillOrderServiceImpl extends CrudServiceImpl<SeckillOrder> implements ISeckillOrderService {
+
+}
