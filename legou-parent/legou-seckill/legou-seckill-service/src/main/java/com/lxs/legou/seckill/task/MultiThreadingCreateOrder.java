5c9f547d3b28146b5d4370b82850e5c3e549090d
tony <tonymarsh0331@gmail.com>
2022-07-06 19:49:25 +0800

    :building_construction: 秒杀微服务，删除测试代码，将Service层中的添加订单代码重构到Task包下的MultiThreadCreateOrder类中

--- legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/task/MultiThreadingCreateOrder.java
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/task/MultiThreadingCreateOrder.java
@@ -1,22 +1,66 @@
 package com.lxs.legou.seckill.task;
 
+import com.lxs.legou.common.utils.IdWorker;
+import com.lxs.legou.common.utils.SystemConstants;
+import com.lxs.legou.seckill.dao.SeckillGoodsDao;
+import com.lxs.legou.seckill.po.SeckillGoods;
+import com.lxs.legou.seckill.po.SeckillOrder;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class MultiThreadingCreateOrder {
 
+
+    private final RedisTemplate redisTemplate;
+    private final SeckillGoodsDao seckillGoodsDao;
+    private final IdWorker idWorker;
+
+    public MultiThreadingCreateOrder(RedisTemplate redisTemplate, SeckillGoodsDao seckillGoodsDao, IdWorker idWorker) {
+        this.redisTemplate = redisTemplate;
+        this.seckillGoodsDao = seckillGoodsDao;
+        this.idWorker = idWorker;
+    }
+
     /**
-     * 多线程测试
+     * 多线程抢单:添加秒杀订单
+     *
+     * @param id       :商品ID
+     * @param time     :商品秒杀开始时间
+     * @param username :⽤户登录名
      */
     @Async
-    public void createOrder() {
-        try {
-            System.out.println("准备执⾏....");
-            Thread.sleep(20000);
-            System.out.println("开始执⾏....");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void createOrder(Long id, String time, String username) {
+        //获取商品数据
+        SeckillGoods goods = (SeckillGoods) redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).get(id);
+        //如果没有库存，则直接抛出异常
+        if (goods == null || goods.getStockCount() <= 0) {
+            throw new RuntimeException("已售罄!");
+        }
+        //如果有库存，则创建秒杀商品订单
+        SeckillOrder seckillOrder = new SeckillOrder();
+        seckillOrder.setId(idWorker.nextId());
+        seckillOrder.setSeckillId(id);
+        seckillOrder.setMoney(goods.getCostPrice());
+        seckillOrder.setUserId(username);
+        seckillOrder.setCreateTime(new Date());
+        seckillOrder.setStatus("0");
+        //将秒杀订单存⼊到Redis中
+        redisTemplate.boundHashOps(SystemConstants.SEC_KILL_ORDER_KEY).put(username, seckillOrder);
+        //库存减少
+        goods.setStockCount(goods.getStockCount() - 1);
+        //判断当前商品是否还有库存
+        if (goods.getStockCount() <= 0) {
+            //并且将商品数据同步到MySQL中
+            seckillGoodsDao.updateById(goods);
+            //如果没有库存,则清空Redis缓存中该商品
+            redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).delete(id);
+        } else {
+            //如果有库存，则将新的商品数据存入Redis中，重置旧数据
+            redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).put(id, goods);
         }
     }
 }
