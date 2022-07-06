8963ba6f96e8963c91a911aa3e0f1c442d5e8505
tony <tonymarsh0331@gmail.com>
2022-07-06 19:06:46 +0800

    :beers: 秒杀微服务，控制层初步实现订单接口

--- legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/controller/SeckillOrderController.java
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/controller/SeckillOrderController.java
@@ -3,6 +3,8 @@ package com.lxs.legou.seckill.controller;
 import com.lxs.legou.core.controller.BaseController;
 import com.lxs.legou.seckill.po.SeckillOrder;
 import com.lxs.legou.seckill.service.ISeckillOrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class SeckillOrderController extends BaseController<ISeckillOrderService, SeckillOrder> {
 
+    private final ISeckillOrderService orderService;
 
+    public SeckillOrderController(ISeckillOrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RequestMapping("/add")
+    public ResponseEntity<String> add(String username, String time, Long id) {
+        try {
+            Boolean success = orderService.add(id, time, username);
+            if (success) return ResponseEntity.ok("抢单成功");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("抢单失败", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
