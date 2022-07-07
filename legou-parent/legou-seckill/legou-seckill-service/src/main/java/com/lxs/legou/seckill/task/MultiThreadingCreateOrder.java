777ef563f73ac30aef6572ad9d01133375502659
tony <tonymarsh0331@gmail.com>
2022-07-07 20:48:57 +0800

    :beers: 秒杀微服务，使用Redisson作为分布式锁的实现，解决多线程秒杀活动下单潜在的超卖问题

--- legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/task/MultiThreadingCreateOrder.java
+++ legou-parent/legou-seckill/legou-seckill-service/src/main/java/com/lxs/legou/seckill/task/MultiThreadingCreateOrder.java
@@ -1,16 +1,20 @@
 package com.lxs.legou.seckill.task;
 
+import com.baomidou.mybatisplus.extension.api.R;
 import com.lxs.legou.common.utils.IdWorker;
 import com.lxs.legou.common.utils.SystemConstants;
 import com.lxs.legou.seckill.dao.SeckillGoodsDao;
 import com.lxs.legou.seckill.po.SeckillGoods;
 import com.lxs.legou.seckill.po.SeckillOrder;
 import com.lxs.legou.seckill.pojo.SeckillStatus;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class MultiThreadingCreateOrder {
@@ -19,11 +23,13 @@ public class MultiThreadingCreateOrder {
     private final RedisTemplate redisTemplate;
     private final SeckillGoodsDao seckillGoodsDao;
     private final IdWorker idWorker;
+    private final RedissonClient redissonClient;
 
-    public MultiThreadingCreateOrder(RedisTemplate redisTemplate, SeckillGoodsDao seckillGoodsDao, IdWorker idWorker) {
+    public MultiThreadingCreateOrder(RedisTemplate redisTemplate, SeckillGoodsDao seckillGoodsDao, IdWorker idWorker, RedissonClient redissonClient) {
         this.redisTemplate = redisTemplate;
         this.seckillGoodsDao = seckillGoodsDao;
         this.idWorker = idWorker;
+        this.redissonClient = redissonClient;
     }
 
     /**
@@ -48,36 +54,49 @@ public class MultiThreadingCreateOrder {
         if (goods == null || goods.getStockCount() <= 0) {
             throw new RuntimeException("已售罄!");
         }
-        //如果有库存，则创建秒杀商品订单
-        SeckillOrder seckillOrder = new SeckillOrder();
-        seckillOrder.setId(idWorker.nextId());
-        seckillOrder.setSeckillId(id);
-        seckillOrder.setMoney(goods.getCostPrice());
-        seckillOrder.setUserId(username);
-        seckillOrder.setCreateTime(new Date());
-        seckillOrder.setStatus("0");
-        //将秒杀订单存⼊到Redis中
-        redisTemplate.boundHashOps(SystemConstants.SEC_KILL_ORDER_KEY).put(username, seckillOrder);
-        //库存减少
-        goods.setStockCount(goods.getStockCount() - 1);
-        //判断当前商品是否还有库存
-        if (goods.getStockCount() <= 0) {
-            //并且将商品数据同步到MySQL中
-            seckillGoodsDao.updateById(goods);
-            //如果没有库存,则清空Redis缓存中该商品
-            redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).delete(id);
-        } else {
-            //如果有库存，则将新的商品数据存入Redis中，重置旧数据
-            redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).put(id, goods);
-        }
-
-        //为方便进行多线程测试，下面模拟耗时操作
+        //分布式锁
+        RLock lock = redissonClient.getLock("seckill_lock_" + id);
+        //同步创建的订单的方法
         try {
-            System.out.println("*******开始模拟耗时操作********");
-            Thread.sleep(10000);
-            System.out.println("*******结束模拟耗时操作********");
-        } catch (InterruptedException e) {
+            //加锁
+            // 参数1：等待锁的(最大)时间，参数2：持有锁的(最大)时间，参数3：时间单位
+            lock.tryLock(20, 20, TimeUnit.SECONDS);
+            //如果有库存，则创建秒杀商品订单
+            SeckillOrder seckillOrder = new SeckillOrder();
+            seckillOrder.setId(idWorker.nextId());
+            seckillOrder.setSeckillId(id);
+            seckillOrder.setMoney(goods.getCostPrice());
+            seckillOrder.setUserId(username);
+            seckillOrder.setCreateTime(new Date());
+            seckillOrder.setStatus("0");
+            //将秒杀订单存⼊到Redis中
+            redisTemplate.boundHashOps(SystemConstants.SEC_KILL_ORDER_KEY).put(username, seckillOrder);
+            //库存减少
+            goods.setStockCount(goods.getStockCount() - 1);
+            //判断当前商品是否还有库存
+            if (goods.getStockCount() <= 0) {
+                //并且将商品数据同步到MySQL中
+                seckillGoodsDao.updateById(goods);
+                //如果没有库存,则清空Redis缓存中该商品
+                redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).delete(id);
+            } else {
+                //如果有库存，则将新的商品数据存入Redis中，重置旧数据
+                redisTemplate.boundHashOps(SystemConstants.SEC_KILL_GOODS_PREFIX + time).put(id, goods);
+            }
+
+            //为方便进行多线程测试，下面模拟耗时操作
+            try {
+                System.out.println("*******开始模拟耗时操作********");
+                Thread.sleep(10000);
+                System.out.println("*******结束模拟耗时操作********");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            //解锁
+            lock.unlock();
         }
 
     }
